306a9293474c1e1d0d59a7ea9dad2213bc7f7995
so-brl <sonia.brunel@le-campus-numerique.fr>
2020-03-26 13:58:30 +0100

    exo 1 ok

--- JS_bases-javascript/exo-1/script.js
+++ JS_bases-javascript/exo-1/script.js
@@ -1,2 +1,13 @@
 // you can write js here
 console.log('hello from file');
+
+//let kelvin = 294 ;
+let kelvin = prompt("Quelle est la température en Kelvin aujourd'hui ?"); // variable qui demande la temperature du jour en Kelvin
+console.log(kelvin);
+
+const celsius = kelvin - 273; // variable qui convertie les Kelvins en Celsius
+console.log(celsius);
+
+const fahrenheit =  Math.floor(celsius * (9/5) + 32 );// vairable qui convertie les  Celsius en fahrenheit
+console.log(fahrenheit);
+
