c0b1a2e224f7890778ea7c5314f575f678178673
so-brl <sonia.brunel@le-campus-numerique.fr>
2020-03-27 16:14:45 +0100

    Exo 4 ok

--- JS_bases-javascript/exo-4/script.js
+++ JS_bases-javascript/exo-4/script.js
@@ -2,20 +2,29 @@
 
 console.log('exo-4');
 
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+let secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
 
 console.log(secretMessage.length); // connaitre la taille du tableau
 
 console.log(secretMessage.pop()); // enleve le dernier éléments du tableau
-
 console.log(secretMessage); // afficher le tableau avec l élément en moins
 
 console.log(secretMessage.push('to','program')); // ajouter des éléments à la fin du tableau
-
 console.log(secretMessage); // afficher le tableau avec les éléments en plus
 
-console.log(secretMessage.indexOf('easily')); //trouver l'index d'une valeur dans un tableau
+let index = secretMessage.indexOf('easily'); //trouver l'index d'une valeur dans un tableau
+console.log(secretMessage.splice(index,1,'right')); //remplacer une valeur à un index précis dans un tableau
+console.log(secretMessage); // afficher le tableau avec les éléments modifié
 
-console.log(secretMessage.splice(secretMessage.indexOf('easily'),1,'right'); //remplacer une valeur à un index précis dans un tableau
 
+console.log(secretMessage.shift());      //supprimer le premier éléments du tableau
+console.log(secretMessage); // afficher le tableau avec les éléments modifié
 
+console.log(secretMessage.unshift("Programming"))//ajouter la chaine "Programming" en début de tableau.
+console.log(secretMessage); // afficher le tableau avec les éléments modifié
+
+console.log(secretMessage.splice(5,5,'know'));// remplacer les mots 'get', 'right', 'the', 'first', 'time' par 'know'.
+console.log(secretMessage); // afficher le tableau avec les éléments modifié
+
+console.log(secretMessage.join(' '));//afficher le message secret à la console.
+console.log(secretMessage); // afficher le tableau avec les éléments modifié
