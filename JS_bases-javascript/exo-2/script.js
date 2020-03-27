b59424e4224baef9fd58a22243fc987d8f9ead37
so-brl <sonia.brunel@le-campus-numerique.fr>
2020-03-27 13:47:33 +0100

    Exo 3 ok

--- JS_bases-javascript/exo-2/script.js
+++ JS_bases-javascript/exo-2/script.js
@@ -2,7 +2,7 @@
 console.log('exo-2');
 
 
-let isInscrit ; // variable de type boolean pour si inscrit
+let isInscrit; // variable de type boolean pour si inscrit
 let age; // du type entier pour l' age du coureur
 const raceNumber = Math.floor(Math.random() * 1000);
 
@@ -11,12 +11,30 @@ const raceNumber = Math.floor(Math.random() * 1000);
 //console.log('Le numéro du joueur sera : ' + raceNumber); //Affiche si le numéro de dossard
 
 
+let runner1 = ['joe',1];
+let runner2 = ['lili',2];
+let newrunner = ['what',3];
+let allRunner = [ [runner1],[runner2] ];
+
+
+function addInscrit( variable ) {
+    for (var i = 0; i < allRunner.length; i++) {
+        if (variable[i] == allRunner[i][0]) {
+            console.log('Déja inscrit');
+        }
+    }
+}
+
+
+addInscrit(newrunner)
+
+
 function run(isInscrit, age) {
     if (isInscrit == true && age < 18) {
         console.log('You will race at 9:30 am');
     } else if (isInscrit == true || age > 18) {
         console.log('You will race at 11:00 am, votre numéro de dossard sera : ' + raceNumber);
-    }else if (isInscrit == false || age < 18) {
+    } else if (isInscrit == false || age < 18) {
         console.log('You will race at 12:30 pm, votre numéro de dossard sera : ' + raceNumber);
     } else {
         console.log('Go see the registration desk');
--- JS_bases-javascript/exo-3/exo3.js
+++ JS_bases-javascript/exo-3/exo3.js
@@ -1,2 +1,88 @@
 // you can write js here
 console.log('exo-3');
+let userInput = prompt("Quelle est votre choix ? \n Choisissez entre :  \n - rock \n - paper \n - scissors  "); // variable qui demande son choix a l'utilisateur
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors';
+const bomb = 'bomb';
+const tied = `Vous êtes à égalité`;
+const won = `Vous avez gangé`;
+const lost = `Vous avez perdu`;
+
+//Récupérer le choix de l'utilisateur
+function getUserChoice(variable) {
+    if (variable === rock || variable === paper || variable === scissors || variable === bomb) {
+        variable.toLowerCase();
+        console.log(`Le choix de l'utilisateur est : ${variable}`);
+        return variable;
+    }
+    let userRetryInput = prompt("Nous n'avons pas compris votre choix. \n Vous devez choisir parmis les réponses suivantes :  \n - rock \n - paper \n - scissors  ");
+    getUserChoice(userRetryInput);
+
+}
+
+//Récupérer le choix du programme.
+function getComputerChoice() {
+    let computerChoice = Math.random(2);
+    if (computerChoice === 0) {
+        console.log(`Le choix de l'ordinateur est : ${rock}`);
+        return rock;
+    } else if (computerChoice === 1) {
+        console.log(`Le choix de l'ordinateur est : ${paper}`);
+        return paper;
+    } else {
+        console.log(`Le choix de l'ordinateur est : ${scissors}`);
+        return scissors;
+    }
+}
+
+// Comparer les choix //Annoncer le gagnant.
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === bomb){
+        console.log(`Vous avez gangé`);
+        return won;
+    } else if (computerChoice === userChoice) {
+        console.log(`Vous êtes à égalité`);
+        return tied;
+    } else if (userChoice === rock) {
+        if (computerChoice === scissors) {
+            console.log(`Vous avez gangé`);
+            return won;
+        } else if (computerChoice === paper) {
+            console.log(`Vous avez perdu`);
+            return lost;
+        }
+    } else if (userChoice === paper && computerChoice === scissors) {
+        console.log(`Vous avez perdu`);
+        return lost;
+    } else if (userChoice === scissors) {
+        if (computerChoice === paper) {
+            console.log(`Vous avez gangé`);
+            return won;
+        }
+        if (computerChoice === rock) {
+            console.log(`Vous avez perdu`);
+            return lost;
+        }
+    }
+}
+
+
+
+
+// commencer le jeux
+
+function playGame() {
+    let uChoice = getUserChoice(userInput);
+    console.log (uChoice);
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    determineWinner(uChoice, computerChoice);
+
+}
+
+
+//let userChoice = getUserChoice(userInput);
+//let computerChoice = getComputerChoice();
+playGame();
+//determineWinner(userChoice, computerChoice);
