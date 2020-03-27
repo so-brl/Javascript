40c73305121e7983e298f5d1884b9b81da1048e4
so-brl <sonia.brunel@le-campus-numerique.fr>
2020-03-27 16:46:49 +0100

    Exo 5 ok

--- JS_bases-javascript/exo-5/exo5.js
+++ JS_bases-javascript/exo-5/exo5.js
@@ -1,3 +1,20 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = 'Turpentine and turtles' ; //Phrase à traduire en paler Baleine (spécial KacDédi à Dorine)
+
+const  vowels = ['a', 'e', 'i', 'o', 'u', 'y']; //Tableau des voyelles de l'alphabet
+
+let resultArray = [] ; // stocker la phrase traduite
+
+for ( i = 0 ; i <input.length ; i++) { // trouver toutes les voyelles dans notre chaine initiale
+    console.log(`la lettre à l'indice ${i} et ${input[i]}`);
+    if (vowels.indexOf(input[i]) > 0 ){
+        resultArray.push(input[i]); // stocker les voyelle dans le tableauresultArray
+        console.log(`et c'est une voyelle`);
+        console.log(resultArray);
+    }
+}
+
+console.log((resultArray.join('')).toUpperCase());
